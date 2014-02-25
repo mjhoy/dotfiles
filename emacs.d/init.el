057216ed50ccc0a66bb8bc4806f72374951e8b53
Michael Hoy <michael.john.hoy@gmail.com>
2014-02-25 07:03:06 -0600

    emacs: js indent 2 spaces

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -44,6 +44,10 @@
 (add-to-list 'auto-mode-alist '("Gemfile\\'"    . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
 
+;; js
+;; ==
+
+(setq js-indent-level 2)
 
 ;; hippy expand
 ;; ============
