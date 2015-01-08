3b78288e2acdf094d5290d8512a6b606b87745e8
Michael Hoy <michael.john.hoy@gmail.com>
2015-01-08 16:49:14 -0500

    emacs: windmove

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -120,6 +120,16 @@
 (put 'narrow-to-region 'disabled nil)
 (put 'narrow-to-page 'disabled nil)
 
+;;; windmove
+
+(windmove-default-keybindings)
+
+;; Make windmove work in org-mode
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
+
 ;;; projectile
 
 (projectile-global-mode)
