808637c7009cd67aaa2a4fd55884e7dfc96f5a42
Michael Hoy <michael.john.hoy@gmail.com>
2015-01-13 07:34:14 -0500

    emacs: checkboxes mark off TODOs automatically as well

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -274,7 +274,27 @@
   (let (org-log-done org-log-states)   ; turn off logging
     (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
 
+;; from https://lists.gnu.org/archive/html/emacs-orgmode/2012-02/msg00515.html
+(defun org-summary-checkboxes ()
+  "Switch entry to DONE when all sub-checkboxes are done, to TODO otherwise."
+  (save-excursion
+    (org-back-to-heading t)
+    (let ((beg (point)) end)
+      (end-of-line)
+      (setq end (point))
+      (goto-char beg)
+      (if (re-search-forward "\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]" end t)
+          (if (match-end 1)
+              (if (equal (match-string 1) "100%")
+                  (org-todo 'done)
+                (org-todo 'todo))
+            (if (and (> (match-end 2) (match-beginning 2))
+                     (equal (match-string 2) (match-string 3)))
+                (org-todo 'done)
+              (org-todo 'todo)))))))
+
 (add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
+(add-hook 'org-checkbox-statistics-hook 'org-summary-checkboxes)
 
 ;;; helm
 
