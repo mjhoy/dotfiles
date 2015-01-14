cef0d1ce89aaa8121e62a1afcd7d0973332fbea6
Michael Hoy <michael.john.hoy@gmail.com>
2015-01-14 16:46:31 -0500

    emacs: php-mode for .module files
    :cough: drupal

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -352,6 +352,8 @@
 
 ;;; php and web-mode
 
+(add-to-list 'auto-mode-alist '("\\.module\\'" . php-mode))
+
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
