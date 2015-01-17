85a5e2ecaa13f1790c0df8f6160720674399b4f7
Michael Hoy <michael.john.hoy@gmail.com>
2015-01-16 20:22:56 -0500

    emacs: dired-get-size

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -144,6 +144,25 @@
 
 (projectile-global-mode)
 
+;;; dired
+
+(require 'dired)
+
+(setq dired-listing-switches "-laGh")
+
+(defun dired-get-size ()
+  (interactive)
+  (let ((files (dired-get-marked-files)))
+    (with-temp-buffer
+      (apply 'call-process "/usr/bin/du" nil t nil "-sch" files)
+      (message
+       "Size of all marked files: %s"
+       (progn
+         (re-search-backward "^\s?\\([0-9.,]+[A-Za-z]+\\).*total$")
+         (match-string 1))))))
+
+(define-key dired-mode-map (kbd "z") 'dired-get-size)
+
 ;;; uniquify
 
 (require 'uniquify)
