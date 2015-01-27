d37613bb2f66c7d1fb228cb59c8cd39811e72d5a
Michael Hoy <mjh@mjhoy.com>
2015-01-26 19:09:12 -0500

    emacs: keep track of current theme and disable when switching

--- emacs.d/lisp/init-theme.el
+++ emacs.d/lisp/init-theme.el
@@ -2,27 +2,38 @@
 (mjhoy/require-package 'color-theme-sanityinc-tomorrow)
 (mjhoy/require-package 'tango-plus-theme)
 
+(defvar mjhoy/current-theme nil)
+
 (load "emacs-boron-theme-mjhoy/boron-theme")
 (load-theme 'boron t)
+(setq mjhoy/current-theme 'boron)
 
 (defun mjhoy/light ()
   "switch to my light theme"
   (interactive)
+  (disable-theme mjhoy/current-theme)
+  (setq mjhoy/current-theme 'tango-plus)
   (load-theme 'tango-plus t))
 
 (defun mjhoy/dark ()
   "switch to my dark theme"
   (interactive)
+  (disable-theme mjhoy/current-theme)
+  (setq mjhoy/current-theme 'sanityinc-tomorrow-night)
   (load-theme 'sanityinc-tomorrow-night t))
 
 (defun mjhoy/bright ()
   "switch to my dark theme (bright)"
   (interactive)
+  (disable-theme mjhoy/current-theme)
+  (setq mjhoy/current-theme 'sanityinc-tomorrow-bright)
   (load-theme 'sanityinc-tomorrow-bright t))
 
 (defun mjhoy/boron ()
   "switch to boron theme"
   (interactive)
+  (disable-theme mjhoy/current-theme)
+  (setq mjhoy/current-theme 'boron)
   (load-theme 'boron t))
 
 (provide 'init-theme)
