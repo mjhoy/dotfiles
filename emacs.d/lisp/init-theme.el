75fd700196b40d23c0fa40e42d1eadedb5e60d31
Michael Hoy <mjh@mjhoy.com>
2015-01-26 19:23:16 -0500

    emacs: mjhoy/switch-theme function; clean up old themes

--- emacs.d/lisp/init-theme.el
+++ emacs.d/lisp/init-theme.el
@@ -1,39 +1,38 @@
 (mjhoy/require-package 'rainbow-mode)
-(mjhoy/require-package 'color-theme-sanityinc-tomorrow)
 (mjhoy/require-package 'tango-plus-theme)
 
 (defvar mjhoy/current-theme nil)
 
+;; custom theme
 (load "emacs-boron-theme-mjhoy/boron-theme")
-(load-theme 'boron t)
-(setq mjhoy/current-theme 'boron)
+
+(defun mjhoy/switch-theme (theme)
+  "Disable currently loaded theme in mjhoy/current-theme,
+  enable THEME and set as mjhoy/current-theme. This allows quick
+  switching between two themes without color pollution. Assume
+  THEME is ok to run lisp code."
+  (interactive
+   (list
+    (intern (completing-read "Load custom theme: "
+			     (mapcar 'symbol-name
+				     (custom-available-themes))))))
+  (unless (custom-theme-name-valid-p theme)
+    (error "Invalid theme name `%s'" theme))
+  (if mjhoy/current-theme
+      (disable-theme mjhoy/current-theme))
+  (setq mjhoy/current-theme theme)
+  (load-theme theme t))
 
 (defun mjhoy/light ()
   "switch to my light theme"
   (interactive)
-  (disable-theme mjhoy/current-theme)
-  (setq mjhoy/current-theme 'tango-plus)
-  (load-theme 'tango-plus t))
+  (mjhoy/switch-theme 'tango-plus))
 
 (defun mjhoy/dark ()
   "switch to my dark theme"
   (interactive)
-  (disable-theme mjhoy/current-theme)
-  (setq mjhoy/current-theme 'sanityinc-tomorrow-night)
-  (load-theme 'sanityinc-tomorrow-night t))
+  (mjhoy/switch-theme 'boron))
 
-(defun mjhoy/bright ()
-  "switch to my dark theme (bright)"
-  (interactive)
-  (disable-theme mjhoy/current-theme)
-  (setq mjhoy/current-theme 'sanityinc-tomorrow-bright)
-  (load-theme 'sanityinc-tomorrow-bright t))
-
-(defun mjhoy/boron ()
-  "switch to boron theme"
-  (interactive)
-  (disable-theme mjhoy/current-theme)
-  (setq mjhoy/current-theme 'boron)
-  (load-theme 'boron t))
+(mjhoy/dark)
 
 (provide 'init-theme)
