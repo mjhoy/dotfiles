253e41a3a3291a22aacf0a51c357048c91c3fb98
Michael Hoy <mjh@mjhoy.com>
2015-02-01 08:43:24 -0500

    emacs: use 4 for c-basic-offset; c-init hook
    - something was overriding c-basic-offset in c mode so I needed to write
      the additional hook.

--- emacs.d/lisp/init-c.el
+++ emacs.d/lisp/init-c.el
@@ -1,7 +1,13 @@
 (setq c-default-style "linux")
-(setq c-basic-offset 6)
-(defun my-make-CR-do-indent ()
+(setq c-basic-offset 4)
+
+(defun mjhoy/c-init ()
+  (setq c-basic-offset 4))
+
+(defun mjhoy/my-make-CR-do-indent ()
   (define-key c-mode-base-map "\C-m" 'c-context-line-break))
-(add-hook 'c-initialization-hook 'my-make-CR-do-indent)
+
+(add-hook 'c-initialization-hook 'mjhoy/my-make-CR-do-indent)
+(add-hook 'c-mode-hook 'mjhoy/c-init)
 
 (provide 'init-c)
