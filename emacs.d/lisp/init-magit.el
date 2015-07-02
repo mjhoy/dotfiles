8e5b6737e9baf0273ca6e942791414cf182fc6bc
Michael Hoy <mjh@mjhoy.com>
2015-07-02 10:06:17 -0400

    copy org-magit script (obsoleted); tweak for magit 2 compat

--- emacs.d/lisp/init-magit.el
+++ emacs.d/lisp/init-magit.el
@@ -1,7 +1,7 @@
 (setq magit-last-seen-setup-instructions "1.4.0")
 
 (mjhoy/require-package 'magit)
-(mjhoy/require-package 'org-magit)
+(require 'org-magit)
 
 (global-set-key (kbd "C-c g") 'magit-status)
 
