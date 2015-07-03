8b6ee59a8957359ce88b2c64599bccaeda6fe06c
Michael Hoy <mjh@mjhoy.com>
2015-07-03 09:06:49 -0400

    emacs: s/ack/the silver searcher/

--- emacs.d/lisp/init-helm.el
+++ emacs.d/lisp/init-helm.el
@@ -25,7 +25,7 @@
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-c h o") 'helm-org-in-buffer-headings)
 (global-set-key (kbd "C-c h e") 'helm-mu)
-(global-set-key (kbd "M-s s") 'helm-projectile-ack)
+(global-set-key (kbd "M-s s") 'helm-projectile-ag)
 
 ;; old buffer switching
 (global-set-key (kbd "C-c h b") 'switch-to-buffer)
