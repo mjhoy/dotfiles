9b2316826bbfd5282792604930db72978602e5ff
Michael Hoy <mjh@mjhoy.com>
2015-07-07 13:55:32 -0400

    emacs: diff-current-buffer-with-file

--- emacs.d/lisp/init-misc.el
+++ emacs.d/lisp/init-misc.el
@@ -7,4 +7,10 @@
 ;; replaces 'view-emacs-debugging
 (global-set-key (kbd "C-h C-d") 'mjhoy/lookup-dash)
 
+(defun mjhoy/diff-current-buffer-with-file ()
+  (interactive)
+  (diff-buffer-with-file (current-buffer)))
+
+(global-set-key (kbd "C-c b d") 'mjhoy/diff-current-buffer-with-file)
+
 (provide 'init-misc)
