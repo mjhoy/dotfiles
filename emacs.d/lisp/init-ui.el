2c912efc70f7f5ddd78cd9bc9658ca170206b73a
Michael Hoy <mjh@mjhoy.com>
2015-07-25 13:01:50 +0000

    emacs: use inconsolata on nix

--- emacs.d/lisp/init-ui.el
+++ emacs.d/lisp/init-ui.el
@@ -1,5 +1,9 @@
-(set-face-attribute 'default nil :family "Input Mono")
-(set-face-attribute 'default nil :height 130)
+(if nixos
+    (progn
+      (set-face-attribute 'default nil :family "Inconsolata")
+      (set-face-attribute 'default nil :height 130))
+  (set-face-attribute 'default nil :family "Input Mono")
+  (set-face-attribute 'default nil :height 130))
 
 (defun mjhoy/proportional ()
   "Use a proportional font"
