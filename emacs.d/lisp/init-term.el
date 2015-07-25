00e18d53402ca8729c11129f1f598eb7f14a67c8
Michael Hoy <mjh@mjhoy.com>
2015-07-25 02:23:59 +0000

    emacs: point to correct bash on nixos

--- emacs.d/lisp/init-term.el
+++ emacs.d/lisp/init-term.el
@@ -1,9 +1,12 @@
 (defun mjhoy/ansi-term (&optional name)
   "Start a new bash ansi-term and ask for a name"
   (interactive (list (read-from-minibuffer "Buffer name: ")))
-  (if (and name (not (string= "" name)))
-      (ansi-term "/bin/bash" (concat name " ansi-term"))
-    (ansi-term "/bin/bash")))
+  (let ((bash-exec (if nixos
+                       "/run/current-system/sw/bin/bash"
+                     "/bin/bash")))
+    (if (and name (not (string= "" name)))
+        (ansi-term bash-exec (concat name " ansi-term"))
+      (ansi-term bash-exec))))
 
 (global-set-key (kbd "C-c t") 'mjhoy/ansi-term)
 
