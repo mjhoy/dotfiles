4a1f96fb2c6f111d7bcdb62901b6d128b4ee9e2c
Michael Hoy <mjh@mjhoy.com>
2015-08-31 08:18:49 -0400

    emacs: make my ansi-term dwim
    if in a sass directory editing scss files, assume standard compass
    layout and dwim.

--- emacs.d/lisp/init-term.el
+++ emacs.d/lisp/init-term.el
@@ -1,14 +1,36 @@
-(defun mjhoy/ansi-term (&optional name)
-  "Start a new bash ansi-term and ask for a name"
-  (interactive (list (read-from-minibuffer "Buffer name: ")))
+(defun mjhoy/ansi-term-dwim (&optional name)
+  "Start a new bash ansi-term and do what I mean.
+
+If the current file is a scss file, and we are in a \"sass\"
+directory, start up a term in the parent directory named
+\"compass ansi-term\". This assumes a standard compass layout and
+that the term is started to run the command `compass watch`.
+
+Otherwise, ask for a name for the new term buffer and start one
+in the current directory.
+"
+  (interactive)
   (let ((bash-exec (if nixos
                        "/run/current-system/sw/bin/bash"
-                     "/bin/bash")))
-    (if (and name (not (string= "" name)))
-        (ansi-term bash-exec (concat name " ansi-term"))
-      (ansi-term bash-exec))))
+                     "/bin/bash"))
+        (dirname (file-name-nondirectory
+                  (directory-file-name default-directory))))
+    ;; compass term
+    (cond ((and (not (null buffer-file-name))
+                (string= "sass" dirname)
+                (string= "scss" (file-name-extension buffer-file-name)))
+           (let ((default-directory
+                  (expand-file-name "../" default-directory)))
+             (ansi-term bash-exec "compass ansi-term")))
+          ;; otherwise: general term
+          (t
+           (let ((name (or name
+                          (read-from-minibuffer "Buffer name: "))))
+             (if (and name (not (string= "" name)))
+                 (ansi-term bash-exec (concat name " ansi-term"))
+               (ansi-term bash-exec)))))))
 
-(global-set-key (kbd "C-c t") 'mjhoy/ansi-term)
+(global-set-key (kbd "C-c t") 'mjhoy/ansi-term-dwim)
 
 (defun mjhoy/term-hooks ()
   ;; yank in ansi term, C-c C-y
