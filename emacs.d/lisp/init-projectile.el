65c4092b8d0f72f8aa8b312797b7567f265d9f08
Michael Hoy <mjh@mjhoy.com>
2015-09-02 17:17:06 -0400

    emacs: mjhoy/projectile-is-drupal

--- emacs.d/lisp/init-projectile.el
+++ emacs.d/lisp/init-projectile.el
@@ -3,6 +3,14 @@
 
 (projectile-global-mode)
 
+(defun mjhoy/projectile-is-drupal ()
+  "Returns t if the current project is a drupal project, nil otherwise."
+  (and (projectile-project-p)
+       (let ((rt (projectile-project-root)))
+         (and (projectile-file-exists-p (concat rt "index.php"))
+              (projectile-file-exists-p (concat rt "sites/all"))
+              (projectile-file-exists-p (concat rt "modules"))))))
+
 ; Disable for the time being as it creates problems with TRAMP
 ;(setq projectile-enable-caching t)
 
