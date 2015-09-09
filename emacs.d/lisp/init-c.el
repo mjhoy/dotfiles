da954461643d7fa256bcd431574ac0aebf2fb1c3
Michael Hoy <mjh@mjhoy.com>
2015-09-09 10:32:29 -0400

    emacs: c: if makefile exists no prompt for compile
    assume the compilation command should just be the default, which is 'make'

--- emacs.d/lisp/init-c.el
+++ emacs.d/lisp/init-c.el
@@ -2,7 +2,10 @@
 (setq c-basic-offset 4)
 
 (defun mjhoy/c-init ()
-  (setq c-basic-offset 4))
+  (setq c-basic-offset 4)
+  (if (or (file-exists-p "makefile")
+          (file-exists-p "Makefile"))
+      (set (make-local-variable 'compilation-read-command) nil)))
 
 (defun mjhoy/my-make-CR-do-indent ()
   (define-key c-mode-base-map "\C-m" 'c-context-line-break))
