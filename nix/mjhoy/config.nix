16cab34171999cc160ee3701aa2844ec6aa941ab
Michael Hoy <mjh@mjhoy.com>
2015-10-05 17:18:07 -0500

    nix: cut back on local config for now

--- nix/mjhoy/config.nix
+++ nix/mjhoy/config.nix
@@ -7,22 +7,8 @@
   #
   # $ nix-env -iA nixpkgs.myHaskellEnv
   packageOverrides = super: let self = super.pkgs; in with self; rec {
-    # haskell package overrides
-    myHaskellPackages = hp: hp.override {
-      overrides = self: super: with pkgs.haskell-ng.lib; {
-        mockery = dontCheck super.mockery;
-      };
-    };
-
-    haskell7101Packages = myHaskellPackages super.haskell-ng.packages.ghc7101;
-
-    myHaskellEnv = buildEnv {
-      name = "myHaskellEnv";
-      paths = with haskell7101Packages; [
-        ghc
-        lens
-        pandoc
-      ];
-    };
+    haskellEnv = haskellPackages.ghcWithPackages (p: with p; [
+      cabal-install
+    ]);
   };
 }
