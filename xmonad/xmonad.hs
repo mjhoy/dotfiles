07f0ccd068f7138381bd71d3633fe1e2d6e17102
Michael Hoy <mjh@mjhoy.com>
2015-10-06 13:25:41 -0500

    xmonad: spawn dmenu at startup

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -6,7 +6,11 @@ myModMask = mod4Mask
 main = xmonad $ defaultConfig {
                   modMask = myModMask
                 , focusedBorderColor = "#000000"
+                , startupHook = startup
                 }
                 `additionalKeys`
                 [ ((myModMask, xK_m), spawn "echo 'Hi, mom!' | dzen2 -p 4")
                 ]
+
+startup = do
+  spawn "dmenu_run"
