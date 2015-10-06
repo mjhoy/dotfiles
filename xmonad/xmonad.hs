ca8e3f1cb30961f16d4e449b696debe8da6d0514
Michael Hoy <mjh@mjhoy.com>
2015-10-05 23:07:41 -0500

    xmonad: make border black

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -3,7 +3,10 @@ import XMonad.Util.EZConfig
 
 myModMask = mod4Mask
 
-main = xmonad $ defaultConfig { modMask = myModMask }
+main = xmonad $ defaultConfig {
+                  modMask = myModMask
+                , focusedBorderColor = "#000000"
+                }
                 `additionalKeys`
                 [ ((myModMask, xK_m), spawn "echo 'Hi, mom!' | dzen2 -p 4")
                 ]
