8bf4f4340cb18e1bc960367079d1001f6ad1bf53
Michael Hoy <mjh@mjhoy.com>
2015-10-09 13:01:05 -0500

    nix: config comment tweak

--- nix/mjhoy/config.nix
+++ nix/mjhoy/config.nix
@@ -5,7 +5,7 @@
 
   # define a derivation i can install/update with
   #
-  # $ nix-env -iA nixpkgs.myHaskellEnv
+  # $ nix-env -f '<nixpkgs>' -iA myHaskellEnv
   packageOverrides = super: let self = super.pkgs; in with self; rec {
     haskellEnv = haskellPackages.ghcWithPackages (p: with p; [
       cabal-install
