8b3646992ccc26e07dc84c78b47e54276783d32a
Michael Hoy <mjh@mjhoy.com>
2015-10-20 09:35:18 -0500

    emacs: functions to toggle haskell-program-name
    this isn't a great solution. basically i'd like nix-shell to set up a
    nice environment if no shell.nix is present; and if it is present then
    just use that.

--- emacs.d/lisp/init-haskell.el
+++ emacs.d/lisp/init-haskell.el
@@ -8,11 +8,27 @@
 (add-hook 'haskell-mode-hook 'mjhoy/define-haskell-keys)
 (add-hook 'haskell-cabal-hook 'mjhoy/define-haskell-cabal-keys)
 
-;; use ghci through nix
-(setq haskell-program-name
-      "nix-shell \
-          -p \"haskellPackages.ghcWithPackages (pkgs: [pkgs.QuickCheck])\" \
+;; TODO: Figure out a better way of starting up a haskell process with
+;; a custom (helpful default) environment when there is no `shell.nix'
+;; present.
+
+;; To use when there is a `shell.nix' in the current directory
+(defun mjhoy/set-haskell-program-clean-nix-shell ()
+  (interactive)
+  (setq haskell-program-name
+        "nix-shell --command 'ghci'")
+  (message haskell-program-name))
+
+;; To use without a `shell.nix' in the current directory
+(defun mjhoy/set-haskell-program-custom-nix-shell ()
+  (interactive)
+  (setq haskell-program-name
+        "nix-shell \
+          -p \"haskellPackages.ghcWithPackages (pkgs: with pkgs; [QuickCheck])\" \
           --command 'ghci'")
+  (message haskell-program-name))
+
+(mjhoy/set-haskell-program-clean-nix-shell)
 
 ;; the following setting will set up haskell-mode to compile with nix-shell:
 ;; (setq haskell-compile-cabal-build-command
