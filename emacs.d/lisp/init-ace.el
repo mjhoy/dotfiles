b33cb0d568e5dcb4f1ffc29b375de3d4b0fb38b4
Michael Hoy <mjh@mjhoy.com>
2015-10-21 15:43:38 -0500

    emacs: ace-window: set keys to home row

--- emacs.d/lisp/init-ace.el
+++ emacs.d/lisp/init-ace.el
@@ -1,6 +1,8 @@
 (mjhoy/require-package 'ace-window)
 (mjhoy/require-package 'avy)
 
+(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+
 ;; previously other-window
 (global-set-key (kbd "C-x o") 'ace-window)
 
