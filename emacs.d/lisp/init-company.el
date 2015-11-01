e82bd8c59f11776fc134b9c393765faa6e7dd1b6
Michael Hoy <mjh@mjhoy.com>
2015-11-01 09:38:02 -0600

    emacs: turn on company by default

--- emacs.d/lisp/init-company.el
+++ emacs.d/lisp/init-company.el
@@ -2,6 +2,7 @@
 
 (add-hook 'after-init-hook 'global-company-mode)
 (global-set-key (kbd "M-SPC") 'company-complete)
-(setq company-idle-delay nil)
+(setq company-idle-delay 0.2)
+(setq company-tooltip-align-annotations t)
 
 (provide 'init-company)
