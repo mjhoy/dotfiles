0244182047b6e86f3cf8925d743c99da22da5fea
Michael Hoy <mjh@mjhoy.com>
2015-11-07 15:40:24 -0600

    emacs: mu4e: remove mailing list col

--- emacs.d/lisp/init-mu4e.el
+++ emacs.d/lisp/init-mu4e.el
@@ -143,7 +143,6 @@
 
       (setq mu4e-headers-fields '((:human-date . 12)
                                   (:flags . 6)
-                                  (:mailing-list . 10)
                                   (:from . 22)
                                   (:thread-subject)))
       (setq mu4e-use-fancy-chars 'threads)
