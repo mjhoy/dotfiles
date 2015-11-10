aac1368281e7141c4253147e5d41029fd996de2b
Michael Hoy <mjh@mjhoy.com>
2015-11-10 11:17:15 -0600

    emacs: org: shortcut to finance.org

--- emacs.d/lisp/init-org/shortcuts.el
+++ emacs.d/lisp/init-org/shortcuts.el
@@ -10,6 +10,7 @@
 (global-set-key (kbd "C-c o b") (openo "belch.org"))
 (global-set-key (kbd "C-c o d") (openo "dates.org"))
 (global-set-key (kbd "C-c o j") (openo "projects.org"))
+(global-set-key (kbd "C-c o f") (openo "finance.org"))
 
 (global-set-key (kbd "C-c o ?") (fni (let ((default-directory org-directory))
                                        (helm-find-files nil))))
