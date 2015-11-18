967d29ef18dbd1264ce73223c817466155584fb1
Michael Hoy <mjh@mjhoy.com>
2015-11-18 13:16:31 -0600

    emacs: mu4e: bring back (small) mailing list col

--- emacs.d/lisp/init-mu4e.el
+++ emacs.d/lisp/init-mu4e.el
@@ -143,6 +143,7 @@
 
       (setq mu4e-headers-fields '((:human-date . 12)
                                   (:flags . 6)
+                                  (:mailing-list . 6)
                                   (:from . 22)
                                   (:thread-subject)))
       (setq mu4e-use-fancy-chars 'threads)
