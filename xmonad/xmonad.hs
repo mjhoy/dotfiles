9f1f06c0f23672c53cb99b95ce45b66d1db8f3e9
Michael Hoy <mjh@mjhoy.com>
2016-01-25 14:10:54 -0600

    xmonad: get the audio keys working

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,16 +1,23 @@
 import XMonad
 import XMonad.Util.EZConfig
 
+xF86AudioRaiseVolume,xF86AudioLowerVolume, xF86AudioMute :: KeySym
+xF86AudioRaiseVolume = 0x1008ff13
+xF86AudioLowerVolume = 0x1008ff11
+xF86AudioMute = 0x1008ff12
+
 myModMask = mod4Mask
 
-main = xmonad $ defaultConfig {
-                  modMask = myModMask
-                , focusedBorderColor = "#000000"
-                , startupHook = startup
-                }
-                `additionalKeys`
-                [ ((myModMask, xK_m), spawn "echo 'Hi, mom!' | dzen2 -p 4")
-                ]
+addl = [ ((myModMask, xK_m), spawn "echo 'Hi, mom!' | dzen2 -p 4")
+       , ((0, xF86AudioLowerVolume), spawn "amixer set Master playback 2.5dB- -q")
+       , ((0, xF86AudioRaiseVolume), spawn "amixer set Master playback 2.5dB+ -q")
+       , ((0, xF86AudioMute), spawn "amixer set Master playback 0% -q")
+       ]
+
+main = xmonad $ defaultConfig { modMask = myModMask
+                              , focusedBorderColor = "#000000"
+                              , startupHook = startup
+                              } `additionalKeys` addl
 
 startup = do
   spawn "dmenu_run"
