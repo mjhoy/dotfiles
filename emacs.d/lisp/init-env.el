440d60c5977547e6f673430fc1a8920902a6abbf
Michael Hoy <mjh@mjhoy.com>
2016-02-11 14:28:49 -0600

    emacs: init-env: note about nixos env vars

--- emacs.d/lisp/init-env.el
+++ emacs.d/lisp/init-env.el
@@ -13,4 +13,22 @@
                (shell-command-to-string "$NIX_GHC --numeric-version")))
       (setenv "NIX_GHC_LIBDIR" "$HOME/.nix-profile/lib/ghc-$NIX_GHC_VERSION" t)))
 
+
+;; nixos:
+;;
+;; emacs (when launched from the gui/xmonad) does not seem to connect
+;; with my ssh-agent, and there may be other issues. i think that
+;; perhaps exec-path-from-shell can solve my issues but i haven't
+;; figured it out yet.
+;;
+;; attempt below, doesn't actually fix it.
+
+;; (mjhoy/require-package 'exec-path-from-shell)
+;; (exec-path-from-shell-initialize)
+;; (exec-path-from-shell-copy-env "INFOPATH")
+;; (exec-path-from-shell-copy-env "NIX_PATH")
+;; (exec-path-from-shell-copy-env "SSH_AGENT_PID")
+;; (exec-path-from-shell-copy-env "SSH_ASKPASS")
+;; (exec-path-from-shell-copy-env "SSH_AUTH_SOCK")
+
 (provide 'init-env)
