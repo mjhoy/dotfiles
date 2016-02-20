b202dd6489a20819f1d5dd281af20ed17ce7b1c3
Michael Hoy <mjh@mjhoy.com>
2016-02-19 21:32:03 -0600

    emacs: use C-n/C-p with company suggestions
    this makes the interface more like helm (i would always get them
    confused)

--- emacs.d/lisp/init-company.el
+++ emacs.d/lisp/init-company.el
@@ -1,8 +1,15 @@
+(require 'init-basic)
 (mjhoy/require-package 'company)
 
 (add-hook 'after-init-hook 'global-company-mode)
 (global-set-key (kbd "M-SPC") 'company-complete)
 (setq company-idle-delay 0.2)
 (setq company-tooltip-align-annotations t)
+
+(define-key company-active-map (kbd "C-n")
+  (fni (company-complete-common-or-cycle 1)))
+
+(define-key company-active-map (kbd "C-p")
+  (fni (company-complete-common-or-cycle -1)))
 
 (provide 'init-company)
