8fc4c1970a4e59378259a252035c6ba2eeb8932d
Michael Hoy <mjh@mjhoy.com>
2016-02-29 09:24:11 -0600

    emacs: conditionally load ledger

--- emacs.d/lisp/init-ledger.el
+++ emacs.d/lisp/init-ledger.el
@@ -1,9 +1,11 @@
-(require 'ledger-mode)
+(if (locate-file "ledger-mode.el" load-path)
+    (progn
+      (require 'ledger-mode)
 
-(defun mjhoy/ledger-setup ()
-  "Setup for ledger mode."
-  (abbrev-mode 1))
+      (defun mjhoy/ledger-setup ()
+        "Setup for ledger mode."
+        (abbrev-mode 1))
 
-(add-hook 'mjhoy/ledger-setup 'ledger-mode-hook)
+      (add-hook 'mjhoy/ledger-setup 'ledger-mode-hook)))
 
 (provide 'init-ledger)
