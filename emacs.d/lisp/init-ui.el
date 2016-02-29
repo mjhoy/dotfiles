510d4c7b0e206675e009c2c28eb8fe528c326ffc
Michael Hoy <mjh@mjhoy.com>
2016-02-28 18:20:45 -0600

    emacs: menlo on osx

--- emacs.d/lisp/init-ui.el
+++ emacs.d/lisp/init-ui.el
@@ -1,6 +1,6 @@
 (if nixos
     (set-face-attribute 'default nil :family "Ubuntu Mono")
-  (set-face-attribute 'default nil :family "Input Mono"))
+  (set-face-attribute 'default nil :family "Menlo"))
 
 (set-face-attribute 'default nil :height 140)
 (set-face-attribute 'default nil :weight 'normal)
