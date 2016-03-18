f1accd7f23ad72c6c327f35e3f897c9b355bc480
Michael Hoy <mjh@mjhoy.com>
2016-03-18 08:06:20 -0500

    emacs: haskell: turn off ghc-mod for now

--- emacs.d/lisp/init-haskell.el
+++ emacs.d/lisp/init-haskell.el
@@ -19,10 +19,10 @@
 ;; ghc-mod setup
 (autoload 'ghc-init "ghc" nil t)
 (autoload 'ghc-debug "ghc" nil t)
-(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+;;(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
 
 ;; ghc-mod company
-(add-to-list 'company-backends 'company-ghc)
+;;(add-to-list 'company-backends 'company-ghc)
 
 ;; use web-mode for snap heist templates
 (add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
