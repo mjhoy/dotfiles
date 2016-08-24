e8d4e401ed385ba7862f064839ede728ebb1e7a6
Michael Hoy <mjh@mjhoy.com>
2016-08-24 11:59:48 -0400

    emacs: web-mode: use for liquid files

--- emacs.d/lisp/init-web-mode.el
+++ emacs.d/lisp/init-web-mode.el
@@ -7,6 +7,7 @@
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.liquid\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
