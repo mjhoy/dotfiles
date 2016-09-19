ac104d34490ab55204aef16cee6b97f94f9bf496
Michael Hoy <mjh@mjhoy.com>
2016-09-19 10:57:30 -0500

    emacs: theme: cleanup

--- emacs.d/lisp/init-theme.el
+++ emacs.d/lisp/init-theme.el
@@ -10,10 +10,6 @@
 (add-to-list 'custom-theme-load-path
              (expand-file-name "site-lisp/davy-theme" user-emacs-directory))
 
-(defun mjhoy/original-theme ()
-  "Disable all custom themes to return to the original theme."
-  (mapcar 'disable-theme custom-enabled-themes))
-
 (defun mjhoy/switch-theme (theme)
   "Disable currently loaded theme in mjhoy/current-theme,
 enable THEME and set as mjhoy/current-theme. This allows quick
@@ -31,7 +27,7 @@ If THEME is `original', disable all custom themes."
   (if (eq 'original theme)
       (progn
         (setq mjhoy/current-theme nil)
-        (mjhoy/original-theme))
+        (mapcar 'disable-theme custom-enabled-themes))
     (unless (custom-theme-name-valid-p theme)
       (error "Invalid theme name `%s'" theme))
     (if mjhoy/current-theme
