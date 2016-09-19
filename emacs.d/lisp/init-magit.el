26d95a294eb86401f2c93a809d7f2ed302198713
Michael Hoy <mjh@mjhoy.com>
2016-09-19 18:08:00 -0500

    emacs: magit: make log of commits to org clock
    if my org clock is running, emacs notes the commits i make in the clock
    item's LOGBOOK, with a link to that commit + the commit message.

--- emacs.d/lisp/init-magit.el
+++ emacs.d/lisp/init-magit.el
@@ -1,6 +1,7 @@
 (setq magit-last-seen-setup-instructions "1.4.0")
 
 (mjhoy/require-package 'magit)
+(require 'init-org)
 (require 'org-magit)
 
 (setq magit-push-always-verify nil)
@@ -8,5 +9,27 @@
 (setq magit-revision-show-gravatars nil)
 
 (global-set-key (kbd "C-c g") 'magit-status)
+
+(defun mjhoy/log-current-commit-to-org-clock ()
+  "Get the current repository's HEAD commit, and add it as a link
+to the current org clock, if one exists."
+  (let* ((repo (magit-toplevel))
+         (rev  (magit-git-string "rev-list" "HEAD" "--abbrev-commit" "-n" "1"))
+         (link (org-magit-make-link repo "::commit@" rev))
+         (summary (magit-git-string "log" "HEAD" "--format=%B" "-n" "1"))
+         (dir  (file-name-nondirectory (directory-file-name repo))))
+    (if (org-clock-is-active)
+        (save-excursion
+          (set-buffer (org-clocking-buffer))
+          (goto-char org-clock-marker)
+          (end-of-line)
+          (newline)
+          (insert "-- ")
+          (insert (format "[[%s][%s.git commit %s]] %s" link dir rev summary))))))
+
+(defun mjhoy/git-commit-hook ()
+  (add-hook 'with-editor-post-finish-hook #'mjhoy/log-current-commit-to-org-clock nil t))
+
+(add-hook 'git-commit-mode-hook #'mjhoy/git-commit-hook)
 
 (provide 'init-magit)
