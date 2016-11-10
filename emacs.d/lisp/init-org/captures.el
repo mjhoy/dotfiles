9ea14c7f64f9fafa104b777c5e982bdc976fd03f
Michael Hoy <mjh@mjhoy.com>
2016-11-10 10:11:47 -0600

    emacs: org: 9.0 compat capture syntax

--- emacs.d/lisp/init-org/captures.el
+++ emacs.d/lisp/init-org/captures.el
@@ -5,7 +5,7 @@
 
 (setq org-capture-templates
       '(("t" "Todo"
-         entry (file+headline (concat org-directory "organizer.org") "General")
+         entry (file+headline (lambda () (concat org-directory "organizer.org")) "General")
          "* TODO %?
 SCHEDULED: %t
 :LOGBOOK:
@@ -14,17 +14,17 @@ SCHEDULED: %t
 
 see: %a\n")
         ("n" "Note"
-         entry (file (concat org-directory "belch.org"))
+         entry (file (lambda () (concat org-directory "belch.org")))
          "* %?
 %U\n%a\n")
         ("c" "Clock" item (clock)
          "%?\n%U\n%a")
-        ("C" "Contact" entry (file (concat org-directory "contacts.org"))
+        ("C" "Contact" entry (file (lambda () (concat org-directory "contacts.org")))
          "* %(org-contacts-template-name)
 :PROPERTIES:
 :EMAIL: %(org-contacts-template-email)
 :END:\n")
-        ("b" "Book" entry (file (concat org-directory "lists/books.org"))
+        ("b" "Book" entry (file (lambda () (concat org-directory "lists/books.org")))
          "* %?
 (C-c C-w to refile to fiction/non-fiction)
 see %a
@@ -32,32 +32,32 @@ entered on %U\n")
         ("q" "Clock (quick)" plain (clock)
          "%a%?\n")
         ("s" "Emacs tool sharpening"
-         entry (file+olp (concat org-directory "programming_notes.org")
+         entry (file+olp (lambda () (concat org-directory "programming_notes.org"))
                          "Emacs"
                          "Sharpening list")
          "* %?
 see %a
 entered on %U\n")
         ("S" "General tool sharpening"
-         entry (file+olp (concat org-directory "programming_notes.org")
+         entry (file+olp (lambda () (concat org-directory "programming_notes.org"))
                          "General sharpening")
          "* %?
 see %a
 entered on %U\n")
         ("d" "Date"
-         entry (file+datetree+prompt (concat org-directory "dates.org"))
+         entry (file+datetree+prompt (lambda () (concat org-directory "dates.org")))
          "* %?
 %t
 
 see %a\n")
         ("j" "Journal"
-         plain (file+datetree (concat org-directory "journal.org"))
+         plain (file+datetree (lambda () (concat org-directory "journal.org")))
          "**** <title>\n%U\n\n%?\n")
         ("r" "Dream"
-         plain (file+datetree (concat org-directory "dream.org"))
+         plain (file+datetree (lambda () (concat org-directory "dream.org")))
          "%?\n")
         ("e" "Engineering journal"
-         plain (file+datetree (concat org-directory "eng_journal.org"))
+         plain (file+datetree (lambda () (concat org-directory "eng_journal.org")))
          "%?\nEntered on %U\n")
         ))
 
