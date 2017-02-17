b8de0fa6fe8e9cadddb0fa65ca733394272388aa
Michael Hoy <mjh@mjhoy.com>
2017-02-17 16:13:40 -0600

    emacs: org: reading notes shortcut

--- emacs.d/lisp/init-org/shortcuts.el
+++ emacs.d/lisp/init-org/shortcuts.el
@@ -7,6 +7,7 @@
 (global-set-key (kbd "C-c o o") (openo "organizer.org"))
 (global-set-key (kbd "C-c o a") (openo "organizer_archive.org"))
 (global-set-key (kbd "C-c o p") (openo "programming_notes.org"))
+(global-set-key (kbd "C-c o r") (openo "reading_notes.org"))
 (global-set-key (kbd "C-c o b") (openo "belch.org"))
 (global-set-key (kbd "C-c o d") (openo "dates.org"))
 (global-set-key (kbd "C-c o j") (openo "projects.org"))
