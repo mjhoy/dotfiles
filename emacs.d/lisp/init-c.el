b838ab47952e97f9389e62507db38182a41114ed
Michael Hoy <mjh@mjhoy.com>
2017-04-18 22:49:24 -0500

    emacs: c: C-c C-c compiles

--- emacs.d/lisp/init-c.el
+++ emacs.d/lisp/init-c.el
@@ -7,10 +7,11 @@
           (file-exists-p "Makefile"))
       (set (make-local-variable 'compilation-read-command) nil)))
 
-(defun mjhoy/my-make-CR-do-indent ()
-  (define-key c-mode-base-map "\C-m" 'c-context-line-break))
+(defun mjhoy/my-c-init ()
+  (define-key c-mode-base-map (kbd "C-m") 'c-context-line-break)
+  (define-key c-mode-base-map (kbd "C-c C-c") 'compile))
 
-(add-hook 'c-initialization-hook 'mjhoy/my-make-CR-do-indent)
+(add-hook 'c-initialization-hook 'mjhoy/my-c-init)
 (add-hook 'c-mode-hook 'mjhoy/c-init)
 
 (provide 'init-c)
