037585e8f52b354826ac5e74eabc5169ea3370c7
Michael Hoy <mjh@mjhoy.com>
2017-06-07 18:48:23 -0500

    emacs: purescript: use different indent algo, decl scan

--- emacs.d/lisp/init-purescript.el
+++ emacs.d/lisp/init-purescript.el
@@ -4,7 +4,9 @@
   "Run to set up purescript mode."
   (psc-ide-mode)
   (flycheck-mode)
-  (turn-on-purescript-indentation))
+  (turn-on-purescript-indent)
+  (turn-on-purescript-decl-scan)
+  )
 
 (add-hook 'purescript-mode-hook 'mjhoy/purescript-mode-setup)
 
