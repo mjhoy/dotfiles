f909c0f00e9a106e7db84ffe7d0a07597a499d76
Michael Hoy <mjh@mjhoy.com>
2017-06-16 11:36:48 -0500

    emacs: remove marmalade repo

--- emacs.d/lisp/init-packages.el
+++ emacs.d/lisp/init-packages.el
@@ -1,7 +1,6 @@
 (require 'package)
 
 (setq package-archives '(("elpa"      . "https://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa"     . "https://melpa.org/packages/")
                          ("org"       . "http://orgmode.org/elpa/")))
 
