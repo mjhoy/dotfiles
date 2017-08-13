9393ffdf8d714aac453850bc7093a60b5436a900
Michael Hoy <mjh@mjhoy.com>
2017-08-13 14:40:53 -0500

    emacs: remove buggy scss package in favor of css mode

--- emacs.d/lisp/init-css.el
+++ emacs.d/lisp/init-css.el
@@ -1,12 +1,11 @@
-(mjhoy/require-package 'scss-mode)
 (mjhoy/require-package 'rainbow-mode)
 
 (setq scss-compile-at-save nil)
 (setq css-indent-offset 2)
 
-(defun mjhoy/init-scss-mode ()
+(defun mjhoy/init-css-mode ()
   (rainbow-mode 1))
 
-(add-hook 'scss-mode-hook 'mjhoy/init-scss-mode)
+(add-hook 'css-mode-hook 'mjhoy/init-css-mode)
 
 (provide 'init-css)
