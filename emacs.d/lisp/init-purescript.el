bef38d8cf4729cbc01a95a5b24ab4c9f28e13da3
Michael Hoy <mjh@mjhoy.com>
2017-09-19 18:44:55 -0500

    emacs: purescript: set psc-ide source globs re psc-package

--- emacs.d/lisp/init-purescript.el
+++ emacs.d/lisp/init-purescript.el
@@ -1,5 +1,6 @@
 (mjhoy/require-package 'psc-ide)
 (mjhoy/require-package 'purescript-mode)
+(mjhoy/require-package 'cl-lib)
 
 (defun mjhoy/purescript-mode-setup ()
   "Run to set up purescript mode."
@@ -8,6 +9,21 @@
   (turn-on-purescript-indentation)
   (turn-on-purescript-decl-scan)
   )
+
+;; This function uses the globs obtained from `psc-package sources` to
+;; set `psc-ide-source-globs'.
+(defun mjhoy/set-psc-ide-sources-for-psc-package ()
+  (interactive)
+  (projectile-with-default-dir (projectile-project-root)
+    (setq psc-ide-source-globs
+          (append
+           '("src/**/*.purs")
+           (cl-remove-if (lambda (s) (equalp "" s))
+                         (split-string
+                          (shell-command-to-string "psc-package sources")
+                          "\n"))
+           )
+          )))
 
 (add-hook 'purescript-mode-hook 'mjhoy/purescript-mode-setup)
 
