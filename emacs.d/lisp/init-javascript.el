86fed7a85453f5d9fb0172ce53cd4454c33321da
Michael Hoy <mjh@mjhoy.com>
2017-11-21 22:39:26 -0600

    emacs: js: add js2 and jsx modes

--- emacs.d/lisp/init-javascript.el
+++ emacs.d/lisp/init-javascript.el
@@ -1,3 +1,8 @@
+(mjhoy/require-package 'js2-mode)
+(mjhoy/require-package 'rjsx-mode)
+
 (setq js-indent-level 2)
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 (provide 'init-javascript)
