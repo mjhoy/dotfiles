5bd4b715cffb385f02aefaaf6078139a7dce8a6c
Michael Hoy <mjh@mjhoy.com>
2017-11-21 22:38:51 -0600

    emacs: web-mode: enable auto-closing

--- emacs.d/lisp/init-web-mode.el
+++ emacs.d/lisp/init-web-mode.el
@@ -11,6 +11,7 @@
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
+(setq web-mode-enable-auto-closing t)
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
