ef4cda2e36086ca272a13aa2ad96e2a54749ff5b
Michael Hoy <mjh@mjhoy.com>
2017-12-07 15:13:56 -0600

    emacs: yafolding: add bindings that work in terminal

--- emacs.d/lisp/init-folding.el
+++ emacs.d/lisp/init-folding.el
@@ -10,4 +10,8 @@
 
 (add-hook 'prog-mode-hook 'mjhoy/init-folding)
 
+(define-key yafolding-mode-map (kbd "C-M-\\") 'yafolding-toggle-all)
+(define-key yafolding-mode-map (kbd "C-S-\\") 'yafolding-hide-parent-element)
+(define-key yafolding-mode-map (kbd "C-\\") 'yafolding-toggle-element)
+
 (provide 'init-folding)
