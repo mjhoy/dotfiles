2c5f43030d1b4bf0b2aed82add27b85c44e5d5c6
Michael Hoy <mjh@mjhoy.com>
2018-01-19 10:34:27 -0600

    nixos: services: note about keybase

--- nix/nixos/common/services.nix
+++ nix/nixos/common/services.nix
@@ -5,4 +5,8 @@
   programs.ssh.startAgent = true;
 
   programs.gnupg.agent.enable = true;
+
+  # TODO: look into getting keybase to work.
+  # See https://github.com/NixOS/nixpkgs/pull/25610
+  # services.kbfs.enable = true;
 }
