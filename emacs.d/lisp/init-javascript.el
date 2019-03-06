c082fa4ab663bc469173e6a9ef098acb5c47cb4f
Michael Hoy <mjh@mjhoy.com>
2019-03-06 17:00:15 -0500

    emacs: js2-mode: no parse errors/warnings

--- emacs.d/lisp/init-javascript.el
+++ emacs.d/lisp/init-javascript.el
@@ -2,6 +2,8 @@
 (mjhoy/require-package 'rjsx-mode)
 
 (setq js-indent-level 2)
+(setq js2-mode-show-parse-errors nil)
+(setq js2-mode-show-strict-warnings nil)
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
