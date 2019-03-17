ce75d8f7bb95854955c01659f0f64d0b54c87b0c
Michael Hoy <mjh@mjhoy.com>
2019-03-17 10:11:03 -0400

    emacs: scala: ensime config

--- emacs.d/lisp/init-scala.el
+++ emacs.d/lisp/init-scala.el
@@ -1,5 +1,16 @@
 (require 'ensime)
 
-(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+;; add the following to ~/.sbt/1.0/plugins/plugins.sbt:
+;;
+;; addSbtPlugin("org.ensime" % "sbt-ensime" % "2.5.1")
+
+(setq ensime-eldoc-hints 'all)
+(setq ensime-typecheck-when-idle nil)
+
+(defun mjhoy/setup-scala-mode ()
+  "My setup for scala-mode."
+  (ensime-mode))
+
+(add-hook 'scala-mode-hook 'mjhoy/setup-scala-mode)
 
 (provide 'init-scala)
