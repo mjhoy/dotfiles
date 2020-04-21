83fe8659fdaba4d897bc45a31b358613ee185aa9
Michael Hoy <mjh@mjhoy.com>
2020-04-21 07:39:41 -0400

    emacs: use lsp for elm

--- emacs.d/lisp/init-elm.el
+++ emacs.d/lisp/init-elm.el
@@ -3,4 +3,11 @@
 (setq elm-format-on-save t)
 (setq elm-sort-imports-on-save t)
 
+(defun mjhoy/setup-elm ()
+  "My setup for elm-mode."
+  (lsp)
+  )
+
+(add-hook 'elm-mode-hook 'mjhoy/setup-elm)
+
 (provide 'init-elm)
