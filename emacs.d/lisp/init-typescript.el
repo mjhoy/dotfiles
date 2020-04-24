6139c8e9ad411aa3d8327f517724a00e52e5c40e
Michael Hoy <mjh@mjhoy.com>
2020-04-24 09:05:06 -0400

    emacs: typescript: clean up config a bit

--- emacs.d/lisp/init-typescript.el
+++ emacs.d/lisp/init-typescript.el
@@ -7,24 +7,20 @@
 
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
 
-(add-hook 'web-mode-hook
-          (lambda ()
-            (when (string-equal "tsx" (file-name-extension buffer-file-name))
-              (setup-tide-mode))))
+
 ;; enable typescript-tslint checker
 (flycheck-add-mode 'typescript-tslint 'web-mode)
 
-(defun setup-tide-mode ()
+(defun mjhoy/setup-tide ()
+  "My setup for tide."
   (interactive)
   (tide-setup)
   (flycheck-mode +1)
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
   (eldoc-mode +1)
   (tide-hl-identifier-mode +1)
-  ;; company is an optional dependency. You have to
-  ;; install it separately via package-install
-  ;; `M-x package-install [ret] company`
-  (company-mode +1))
+  (company-mode +1)
+  )
 
 ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
@@ -33,5 +29,9 @@
 (add-hook 'before-save-hook 'tide-format-before-save)
 
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (mjhoy/setup-tide))))
 
 (provide 'init-typescript)
