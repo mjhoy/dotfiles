32f46d92bd0cef69f24d283707ac28c704b7fe89
Michael Hoy <mjh@mjhoy.com>
2020-04-24 07:09:24 -0400

    emacs: org: open programming_notes directory on shortcut

--- emacs.d/lisp/init-org/shortcuts.el
+++ emacs.d/lisp/init-org/shortcuts.el
@@ -1,12 +1,13 @@
-(defun mjhoy/open-org-notebook (filename)
-  (find-file (concat org-directory filename)))
+(defun mjhoy/open-org-notebook (filename &optional use-helm)
+  (let ((find-func (if use-helm #'helm-find-files-1 #'find-file)))
+    (funcall find-func (concat org-directory filename))))
 
 (defmacro openo (&rest forms)
   `(fni (mjhoy/open-org-notebook ,@forms)))
 
 (global-set-key (kbd "C-c o o") (openo "organizer.org"))
 (global-set-key (kbd "C-c o a") (openo "organizer_archive.org"))
-(global-set-key (kbd "C-c o p") (openo "programming_notes.org"))
+(global-set-key (kbd "C-c o p") (openo "programming_notes/" t))
 (global-set-key (kbd "C-c o r") (openo "reading_notes.org"))
 (global-set-key (kbd "C-c o b") (openo "belch.org"))
 (global-set-key (kbd "C-c o d") (openo "dates.org"))
