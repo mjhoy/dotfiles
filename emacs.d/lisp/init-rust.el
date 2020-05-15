c050f866c0cd51505d9d1ca05b19a63260b2fed2
Michael Hoy <mjh@mjhoy.com>
2020-05-15 05:45:10 -0400

    emacs: rust: use rust-analyzer

--- emacs.d/lisp/init-rust.el
+++ emacs.d/lisp/init-rust.el
@@ -4,10 +4,13 @@
 
 (add-hook 'rust-mode-hook #'flycheck-rust-setup)
 
+(setq lsp-rust-server 'rust-analyzer)
+
 (defun mjhoy/init-rust-mode ()
   "Set up rust mode"
   (lsp)
   (setq rust-format-on-save t)
+  (cargo-minor-mode)
   )
 
 (add-hook 'rust-mode-hook 'mjhoy/init-rust-mode)
