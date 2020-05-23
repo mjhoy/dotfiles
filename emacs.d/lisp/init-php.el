fe379773031525a766148fb49ab08f926403c3de
Michael Hoy <mjh@mjhoy.com>
2020-05-23 17:20:04 -0400

    emacs: php: sane indentation

--- emacs.d/lisp/init-php.el
+++ emacs.d/lisp/init-php.el
@@ -6,6 +6,14 @@
 
 (add-hook 'php-mode-hook 'php-enable-drupal-coding-style)
 
+(defun mjhoy/setup-php-mode ()
+  "Setup for PHP mode."
+  (setq c-basic-offset 2)
+  (php-enable-drupal-coding-style)
+  )
+
+(add-hook 'php-mode-hook 'mjhoy/setup-php-mode)
+
 ;; geben init?
 
 (provide 'init-php)
