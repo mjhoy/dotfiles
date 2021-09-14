47649b8bcf7611083bdfcecaa44403ecf0f62443
Michael Hoy <mjh@mjhoy.com>
2021-09-13 21:39:59 -0400

    emacs: typescript: use lsp-mode

--- emacs.d/lisp/init-typescript.el
+++ emacs.d/lisp/init-typescript.el
@@ -1,6 +1,5 @@
 (require 'web-mode)
 (require 'typescript-mode)
-(require 'tide)
 (require 'init-flycheck)
 
 (add-to-list 'auto-mode-alist '("\\.ts\\'" .  typescript-mode))
@@ -11,27 +10,16 @@
 ;; enable typescript-tslint checker
 (flycheck-add-mode 'typescript-tslint 'web-mode)
 
-(defun mjhoy/setup-tide ()
-  "My setup for tide."
+(defun mjhoy/setup-typescript-mode ()
+  "My setup for typescript."
   (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  (company-mode +1)
+  (lsp)
   )
 
-;; aligns annotation to the right hand side
-(setq company-tooltip-align-annotations t)
-
-;; formats the buffer before saving
-(add-hook 'before-save-hook 'tide-format-before-save)
-
-(add-hook 'typescript-mode-hook #'mjhoy/setup-tide)
+(add-hook 'typescript-mode-hook #'mjhoy/setup-typescript-mode)
 (add-hook 'web-mode-hook
           (lambda ()
             (when (string-equal "tsx" (file-name-extension buffer-file-name))
-              (mjhoy/setup-tide))))
+              (mjhoy/setup-typescript-mode))))
 
 (provide 'init-typescript)
