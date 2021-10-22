90644dea7b3160e1bf8a992d3e91e8626d89573b
Michael Hoy <mjh@mjhoy.com>
2021-10-22 11:07:20 -0400

    emacs: lsp: enable lsp-ui-doc

--- emacs.d/lisp/init-lsp.el
+++ emacs.d/lisp/init-lsp.el
@@ -7,7 +7,7 @@
 
 (setq lsp-prefer-flymake nil)
 
-(setq lsp-ui-doc-enable nil)
+(setq lsp-ui-doc-enable t)
 (setq lsp-ui-peek-enable nil)
 (setq lsp-ui-sideline-show-diagnostics nil)
 (setq lsp-ui-sideline-show-hover nil)
