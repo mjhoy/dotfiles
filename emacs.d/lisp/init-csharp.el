4399fe942ac6bb5db53cce86fd95c74954744587
Michael Hoy <mjh@mjhoy.com>
2021-10-28 15:40:58 -0400

    emacs: csharp: run test bindings

--- emacs.d/lisp/init-csharp.el
+++ emacs.d/lisp/init-csharp.el
@@ -5,6 +5,8 @@
   "My setup for csharp-mode."
   (lsp)
   (add-hook 'after-save-hook #'lsp-format-buffer nil t)
+  (local-set-key (kbd "C-c C-t") #'lsp-csharp-run-test-at-point)
+  (local-set-key (kbd "C-c M-t") #'lsp-csharp-run-all-tests-in-buffer)
   )
 
 (add-hook 'csharp-mode-hook #'mjhoy/setup-csharp-mode)
