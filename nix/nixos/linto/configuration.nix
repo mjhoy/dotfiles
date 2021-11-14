0b97b4a0b5981610953e19ff7e5ee293b4e17374
Michael Hoy <mjh@mjhoy.com>
2021-11-14 05:19:57 -0500

    nixos: linto: set default locale
    I don't think this actually changed anything. Seems good to do?

--- nix/nixos/linto/configuration.nix
+++ nix/nixos/linto/configuration.nix
@@ -37,7 +37,7 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
   # console = {
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
