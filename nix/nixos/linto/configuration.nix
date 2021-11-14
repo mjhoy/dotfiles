eb2858d60b42e2dd7dc82ef6576090de54cdf919
Michael Hoy <mjh@mjhoy.com>
2021-11-14 05:18:47 -0500

    nixos: linto: update hostname

--- nix/nixos/linto/configuration.nix
+++ nix/nixos/linto/configuration.nix
@@ -17,7 +17,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "linto";
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Set your time zone.
