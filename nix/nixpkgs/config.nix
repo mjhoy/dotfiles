2e17c7e08576bf15be7ce6a78e528a7f77849550
Michael Hoy <mjh@mjhoy.com>
2021-12-20 21:12:32 -0500

    nix: update my linux env

--- nix/nixpkgs/config.nix
+++ nix/nixpkgs/config.nix
@@ -23,11 +23,17 @@
         clang
 
         # other
-        mdk
-        pinentry
-        vnstat
-        docker_compose
         bmon
+        coreutils
+        docker_compose
+        dotnet-sdk_5
+        mariadb-client
+        mdk
+        nodejs-14_x
+        omnisharp-roslyn
+        pinentry
+        unzip
+        vnstat
       ];
     };
 
@@ -41,7 +47,7 @@
       paths = [
         firefox
         slack
-        gimp
+        zoom-us
       ];
     };
 
