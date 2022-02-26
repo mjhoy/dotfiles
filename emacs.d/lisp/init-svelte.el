869698c7911d9f4784701c93e9041ad6dc94cd43
Michael Hoy <mjh@mjhoy.com>
2022-02-26 05:46:42 -0500

    emacs: svelte: hacky workaround for prettier cursor bug

--- emacs.d/lisp/init-svelte.el
+++ emacs.d/lisp/init-svelte.el
@@ -3,6 +3,22 @@
 
 (add-to-list 'auto-mode-alist '("\\.svelte\\'" . web-mode))
 
+(defvar mjhoy/hacky-svelte-save-point-value 0)
+
+(defun mjhoy/hacky-svelte-save-point ()
+  "Save the current cursor position, to restore later.
+
+A big fat hack for getting around an issue with prettier and svelte/web-mode
+always setting the cursor back to the top of the file.
+"
+  (setq-local mjhoy/hacky-svelte-save-point-value (point))
+  )
+
+(defun mjhoy/hacky-svelte-restore-point ()
+  "Restore the point for the svelte/web-mode hack."
+  (goto-char mjhoy/hacky-svelte-save-point-value)
+  )
+
 (defun mjhoy/init-svelte-mode ()
   "My set up for svelt-mode."
 
@@ -14,6 +30,10 @@
     (if is-svelte-file
         (progn
           (lsp)
+
+          ;; Add the hacky cursor save/restore hooks.
+          (add-hook 'before-save-hook 'mjhoy/hacky-svelte-save-point -100 t)
+          (add-hook 'after-save-hook 'mjhoy/hacky-svelte-restore-point 100 t)
           )))
   )
 
