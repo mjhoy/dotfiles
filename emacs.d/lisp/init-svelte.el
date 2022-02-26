4cb5d71395937d2faf5f4b4420332ebbae210994
Michael Hoy <mjh@mjhoy.com>
2022-02-26 05:05:33 -0500

    emacs: svelte: use lsp
    This *works* but I don't love the init hook, since what we're using is
    web-mode, not svelte-mode.

--- emacs.d/lisp/init-svelte.el
+++ emacs.d/lisp/init-svelte.el
@@ -1,5 +1,22 @@
-(require 'web-mode)
+(require 'init-web-mode)
+(require 'init-lsp)
 
 (add-to-list 'auto-mode-alist '("\\.svelte\\'" . web-mode))
+
+(defun mjhoy/init-svelte-mode ()
+  "My set up for svelt-mode."
+
+  ;; We're actually in web-mode. So check to make sure we're in a
+  ;; svelte file. This seems a bit hacky?
+  (let
+      ((case-fold-search nil)
+       (is-svelte-file (string-match "\\.SVELTE\\'" buffer-file-name)))
+    (if is-svelte-file
+        (progn
+          (lsp)
+          )))
+  )
+
+(add-hook 'web-mode-hook 'mjhoy/init-svelte-mode)
 
 (provide 'init-svelte)
