3de32b2cfba9e9ac793b923f0e8d2b16d116c013
Mikey Hoy <mjh@mjhoy.com>
2022-10-03 07:58:48 -0400

    emcas: org: first stab at a "project" agena view

--- emacs.d/lisp/init-org/agenda.el
+++ emacs.d/lisp/init-org/agenda.el
@@ -10,6 +10,32 @@
 (setq org-refile-targets
       `((,(concat org-directory "projects.org") :level . 1)))
 
+;; the "Project" view. adapted from the agenda setup here:
+;; https://www.labri.fr/perso/nrougier/GTD/index.html#org8c6ecc6
+(setq org-agenda-custom-commands
+      '(("j" "Projects"
+         ((agenda ""
+                  ((org-agenda-skip-function
+                    '(org-agenda-skip-entry-if 'deadline))
+                   (org-deadline-warning-days 0)))
+          (todo "NEXT"
+                ((org-agenda-skip-function
+                  '(org-agenda-skip-entry-if 'deadline))
+                 (org-agenda-prefix-format "  %i %-12:c [%e] ")
+                 (org-agenda-overriding-header "\nTasks\n")))
+          (agenda nil
+                  ((org-agenda-entry-types '(:deadline))
+                   (org-agenda-format-date "")
+                   (org-deadline-warning-days 7)
+                   (org-agenda-skip-function
+                    '(org-agenda-skip-entry-if 'notregexp "\\* NEXT"))
+                   (org-agenda-overriding-header "\nDeadlines")))
+          (tags-todo "inbox"
+                     ((org-agenda-prefix-format "  %?-12t% s")
+                      (org-agenda-overriding-header "\nInbox\n")))
+          (tags "CLOSED>=\"<today>\""
+                ((org-agenda-overriding-header "\nCompleted today\n")))))))
+
 (setq org-agenda-dim-blocked-tasks nil)
 
 (setq org-agenda-sticky t)
