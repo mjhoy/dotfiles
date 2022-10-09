a61354bf2fc8aab37ae29c95545e2742d22ee8c7
Mikey Hoy <mjh@mjhoy.com>
2022-10-09 05:48:16 -0400

    emacs: support avy search in isearch

--- emacs.d/lisp/init-avy.el
+++ emacs.d/lisp/init-avy.el
@@ -5,4 +5,7 @@
 (global-set-key (kbd "C-c ;") 'avy-goto-char-2)
 (global-set-key (kbd "C-c '") 'avy-goto-line)
 
+(eval-after-load "isearch"
+  '(define-key isearch-mode-map (kbd "C-c ;") 'avy-isearch))
+
 (provide 'init-avy)
