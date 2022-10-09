ca04ccf8d930d41594c378b03667ab26b3725389
Mikey Hoy <mjh@mjhoy.com>
2022-10-09 05:34:16 -0400

    emacs: use avy-goto-char-2

--- emacs.d/lisp/init-ace.el
+++ emacs.d/lisp/init-ace.el
@@ -2,7 +2,7 @@
 
 (global-set-key (kbd "C-x p") 'ace-window)
 
-(global-set-key (kbd "C-c ;") 'avy-goto-char)
+(global-set-key (kbd "C-c ;") 'avy-goto-char-2)
 (global-set-key (kbd "C-c '") 'avy-goto-line)
 
 (provide 'init-ace)
