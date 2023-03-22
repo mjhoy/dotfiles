53791dd20af32110310124ac5eaef52a5398e2bb
Mikey Hoy <mjh@mjhoy.com>
2023-03-22 18:36:54 -0400

    nixpkgs: bump emacs overlay (gptel is newish)

--- nix/nixpkgs/overlays.nix
+++ nix/nixpkgs/overlays.nix
@@ -3,6 +3,6 @@
     # To update:
     # Go to https://github.com/nix-community/emacs-overlay, and grab a commit hash.
     # Swap it in below.
-    url = https://github.com/nix-community/emacs-overlay/archive/545383bd7de8e3f100356fea217698379d8f5c31.tar.gz;
+    url = https://github.com/nix-community/emacs-overlay/archive/446ad9be4447bdadd32120d1983ec8510c5e7232.tar.gz;
   }))
 ]
