f5df51babf6d48d8ca4d6b3cd61769dbb1e13152
Mikey Hoy <mjh@mjhoy.com>
2023-09-21 09:26:11 -0400

    emacs: markdown: enable auto-fill-mode

--- emacs.d/lisp/init-markdown.el
+++ emacs.d/lisp/init-markdown.el
@@ -1,3 +1,10 @@
 (require 'markdown-mode)
 
+(defun mjhoy/setup-markdown-mode ()
+  "My setup for markdown-mode."
+  (auto-fill-mode 1)
+  )
+
+(add-hook 'markdown-mode-hook #'mjhoy/setup-markdown-mode)
+
 (provide 'init-markdown)
