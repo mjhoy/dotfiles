6121eda76b2ab397b35910636e442cc2038ba6a8
Mikey Hoy <mjh@mjhoy.com>
2023-10-16 10:19:48 -0400

    bump emacs overlay 2023-10-16

--- nix/nixpkgs/overlays.nix
+++ nix/nixpkgs/overlays.nix
@@ -3,6 +3,6 @@
     # To update:
     # Go to https://github.com/nix-community/emacs-overlay, and grab a commit hash.
     # Swap it in below.
-    url = https://github.com/nix-community/emacs-overlay/archive/54087a1b3a7ef51d0968bbfa7a700d2848ffae9c.tar.gz;
+    url = https://github.com/nix-community/emacs-overlay/archive/67acc54cceef38a92ba132e121659fef97698184.tar.gz;
   }))
 ]
