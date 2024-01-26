32ac3de5eef714158beafbf6eb5a208983f34ec4
Mikey Hoy <mjh@mjhoy.com>
2024-01-25 19:46:55 -0800

    emacs: flymake: init with goto- shortcuts

---
+++ emacs.d/lisp/init-flymake.el
@@ -0,0 +1,6 @@
+(require 'flymake)
+
+(define-key flymake-mode-map (kbd "M-n") 'flymake-goto-next-error)
+(define-key flymake-mode-map (kbd "M-p") 'flymake-goto-prev-error)
+
+(provide 'init-flymake)
