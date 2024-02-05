c6068dbd1d6f86be87c166924a664b90f0ed22f3
Mikey Hoy <mjh@mjhoy.com>
2024-02-05 05:42:08 -0800

    emacs: default to modus-vivendi-tinted 😎

--- emacs.d/lisp/init-theme.el
+++ emacs.d/lisp/init-theme.el
@@ -31,6 +31,6 @@ If THEME is `original', disable all custom themes."
     (setq mjhoy/current-theme theme)
     (load-theme theme t)))
 
-(mjhoy/switch-theme 'sanityinc-tomorrow-night)
+(mjhoy/switch-theme 'modus-vivendi-tinted)
 
 (provide 'init-theme)
