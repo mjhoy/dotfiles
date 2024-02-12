8316bfdac55224d317c61842100b474c2b10b4e1
Mikey Hoy <mjh@mjhoy.com>
2024-02-12 04:46:11 -0800

    emacs: eglot: add eglot-rename binding

--- emacs.d/lisp/init-eglot.el
+++ emacs.d/lisp/init-eglot.el
@@ -7,4 +7,7 @@
 (defun mjhoy/eglot-organize-imports () (interactive)
        (eglot-code-actions nil nil "source.organizeImports" t))
 
+(with-eval-after-load 'eglot
+  (define-key eglot-mode-map (kbd "C-c C-r") 'eglot-rename))
+
 (provide 'init-eglot)
