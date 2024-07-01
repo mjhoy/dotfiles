770b05ecd39eb62a15cc6f2fe70708d6b5d40d46
Anka <runner@Mac-1719823596123.local>
2024-07-01 09:05:07 +0000

    update emacs overlay 2024-07-01

--- nix/nixpkgs/overlays.nix
+++ nix/nixpkgs/overlays.nix
@@ -3,6 +3,6 @@
     # To update:
     # Go to https://github.com/nix-community/emacs-overlay, and grab a commit hash.
     # Swap it in below.
-    url = https://github.com/nix-community/emacs-overlay/archive/08dd5fc2b327774d7b605a39273b23af7d4af08f.tar.gz;
+    url = https://github.com/nix-community/emacs-overlay/archive/9dd348fbc5c9708a607cda51b70958491bf925b3.tar.gz;
   }))
 ]
