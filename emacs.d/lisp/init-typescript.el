be6e7d6323c9402899ae46672cc7275388f25ef3
Mikey Hoy <mjh@mjhoy.com>
2024-08-05 14:15:36 -0400

    emacs: support for non-tsx ts-mode in hook

--- emacs.d/lisp/init-typescript.el
+++ emacs.d/lisp/init-typescript.el
@@ -9,13 +9,14 @@
 
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))
 
-(defun mjhoy/setup-tsx-ts-mode ()
-  "My setup for tsx-ts-mode."
+(defun mjhoy/setup-typescript-ts-mode ()
+  "My setup for tsx-ts-mode and typescript-ts-mode."
   (eglot-ensure)
   (add-hook 'before-save-hook #'prettier-prettify nil t)
   )
 
-(add-hook 'tsx-ts-mode-hook 'mjhoy/setup-tsx-ts-mode)
+(add-hook 'tsx-ts-mode-hook 'mjhoy/setup-typescript-ts-mode)
+(add-hook 'typescript-ts-mode-hook 'mjhoy/setup-typescript-ts-mode)
 
 ;; enable typescript-tslint checker
 (flycheck-add-mode 'typescript-tslint 'web-mode)
