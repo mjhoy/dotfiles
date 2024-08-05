b92420774936729f73b765c953f79fcf79473c50
Mikey Hoy <mjh@mjhoy.com>
2024-08-05 08:34:08 -0400

    emacs: winring: add global next/prev bindings (command-<left/right>)

--- emacs.d/lisp/init-winring.el
+++ emacs.d/lisp/init-winring.el
@@ -1,4 +1,7 @@
 (require 'winring)
 (winring-initialize)
 
+(global-set-key (kbd "s-<right>") 'winring-next-configuration)
+(global-set-key (kbd "s-<left>") 'winring-prev-configuration)
+
 (provide 'init-winring)
