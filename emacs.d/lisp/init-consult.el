c82e94f72b537809b1b3886c52765d3ece521ee6
Mikey Hoy <mjh@mjhoy.com>
2024-09-30 05:33:46 -0400

    emacs: more consult keybindings

--- emacs.d/lisp/init-consult.el
+++ emacs.d/lisp/init-consult.el
@@ -2,9 +2,17 @@
 
 ;; Original: switch-to-buffer
 (global-set-key (kbd "C-x b") 'consult-buffer)
-(global-set-key (kbd "C-c p s") 'consult-ripgrep)
+;; Original: goto-line
+(global-set-key (kbd "M-g g") 'consult-goto-line)
+
+;; Searching in projects
+(global-set-key (kbd "C-c p s s") 'consult-ripgrep)
+(global-set-key (kbd "C-c p s l") 'consult-line-multi)
+(global-set-key (kbd "C-c p s i") 'consult-imenu-multi)
+
 (global-set-key (kbd "C-c p f") 'consult-fd)
 (global-set-key (kbd "C-c p b") 'consult-project-buffer)
-(global-set-key (kbd "C-c C-s") 'consult-line)
+(global-set-key (kbd "C-c s") 'consult-line)
+(global-set-key (kbd "C-c e") 'consult-flymake)
 
 (provide 'init-consult)
