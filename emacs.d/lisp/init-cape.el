f3292bd25ab11f9f957f825b312e3b974d753131
Mikey Hoy <mjh@mjhoy.com>
2024-10-03 06:59:58 -0400

    emacs: add cape-emoji completions 🐢

--- emacs.d/lisp/init-cape.el
+++ emacs.d/lisp/init-cape.el
@@ -1,5 +1,6 @@
 (require 'cape)
 
-(add-hook 'completion-at-point-functions #'cape-dict)
+(add-to-list 'completion-at-point-functions #'cape-dict)
+(add-to-list 'completion-at-point-functions #'cape-emoji)
 
 (provide 'init-cape)
