e502c00ccf4bc17a28d3641b6369d80db4616db1
Mikey Hoy <mjh@mjhoy.com>
2024-11-06 08:01:33 -0500

    emacs: add linear package as borg drone

--- emacs.d/lisp/init-linear.el
+++ emacs.d/lisp/init-linear.el
@@ -1,7 +1,1 @@
-(if (file-exists-p (expand-file-name "~/proj/linear-emacs"))
-    (progn
-      (add-to-list 'load-path (expand-file-name "~/proj/linear-emacs"))
-      (require 'linear)
-      ))
-
 (provide 'init-linear)
