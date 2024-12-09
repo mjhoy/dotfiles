236e8312804542bb40c61eb058ee99f990701342
Mikey Hoy <mjh@mjhoy.com>
2024-12-09 06:39:01 -0500

    fish: truncate long git branch names in prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,8 @@
 set -g fish_greeting
 
+# Truncate long git branch names in the prompt
+set -g __fish_git_prompt_shorten_branch_len 20
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
