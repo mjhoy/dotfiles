c2047f7781e5a82e09e9c4c28df6dbfa62860022
Mikey Hoy <mjh@mjhoy.com>
2025-07-29 06:04:47 -0400

    emacs: project-query-replace global binding

--- emacs.d/lisp/init-project.el
+++ emacs.d/lisp/init-project.el
@@ -2,6 +2,7 @@
 
 (global-set-key (kbd "C-c p p") 'project-switch-project)
 (global-set-key (kbd "C-c p &") 'project-async-shell-command)
+(global-set-key (kbd "C-c p r") 'project-query-replace-regexp)
 
 (setopt project-vc-extra-root-markers '(".project"))
 
