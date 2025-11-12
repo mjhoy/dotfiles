27577341d3f60ed2d43dfb1949f9da3d7f940227
Mikey Hoy <mjh@mjhoy.com>
2025-11-12 09:39:00 -0500

    fish: conditionally load work-specific config

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,9 @@ set -g __fish_git_prompt_shorten_branch_len 20
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Work-specific stuff goes in ~/.config/fish/work.fish
+if test (hostname) = "plumbeous"
+    and test -f ~/.config/fish/work.fish
+    source ~/.config/fish/work.fish
+end
